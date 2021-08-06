c980a75f8136b5a67acd445e9a67c7b81cdcf85d
Jacob C <45279415+jacobc2700@users.noreply.github.com>
2021-08-06 12:39:59 -0400

    changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className='footer'>
         <a
           href='https://github.com/jacobc2700'
-          data-tooltip='GitHub'
+          data-tooltip='GITHUB'
           data-tooltip-location='top'
           target='_blank'
           rel='noopener noreferrer'
@@ -14,17 +14,8 @@ const Footer = () => {
           <i className='footer-icon fa fa-github'></i>
         </a>
         <a
-          href='https://www.youtube.com/c/Humboo'
-          data-tooltip='YouTube'
-          data-tooltip-location='top'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <i className='footer-icon fa fa-youtube-play'></i>
-        </a>
-        <a
           href='https://www.linkedin.com/in/jacob-chang-94a695191/'
-          data-tooltip='LinkedIn'
+          data-tooltip='LINKEDIN'
           data-tooltip-location='top'
           target='_blank'
           rel='noopener noreferrer'
@@ -32,8 +23,8 @@ const Footer = () => {
           <i className='footer-icon fa fa-linkedin'></i>
         </a>
         <a
-          href='mailto:jechang3@illinois.edu'
-          data-tooltip='Email'
+          href='mailto:jacobchang124@gmail.com'
+          data-tooltip='EMAIL'
           data-tooltip-location='top'
         >
           <i className='footer-icon fa fa-envelope'></i>
